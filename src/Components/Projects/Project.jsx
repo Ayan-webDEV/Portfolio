715cad7c69f4989c672da44ddebf6c5d0811b792
Ayan-webDEV <ayanalamdev@gmail.com>
2025-11-09 21:08:31 +0530

    project section introduced partially

---
+++ src/Components/Projects/Project.jsx
@@ -0,0 +1,141 @@
+import style from "./Project.module.css";
+// import { useTheme } from "../../ContextStore/ContextStore";
+import Hedding from "../CommonComponent/Hedding";
+import Divider from "../CommonComponent/Divider";
+
+const Project = () => {
+  //   const { darkMode } = useTheme();
+
+  return (
+    <>
+      <section
+        id="projects"
+        className="container-fluid my-4 pt-3 my-sm-5 mt-lg-4 mb-lg-5 pt-lg-4"
+      >
+        <div className="container">
+          <Hedding hds={"Projects"} cursor={"⌘"} />
+
+          <div className="mt-5 mt-lg-4">
+            <h4 className={`${style.projectHdsCommon} ${style.projectHdsMob}`}>
+              ⚡ Turning Ideas <br />
+              <span style={{ color: "var(--text-color)" }}>
+                into
+              </span> <br /> Impactful Code
+            </h4>
+            <h4
+              className={`${style.projectHdsCommon} ${style.projectHdsTblet}`}
+            >
+              ⚡ Turning Creative Ideas <br />
+              <span style={{ color: "var(--text-color)" }}>
+                into
+              </span> <br /> Impactful Code
+            </h4>
+            <h4 className={`${style.projectHdsCommon} ${style.projectHds}`}>
+              ⚡ Turning Ideas{" "}
+              <span style={{ color: "var(--text-color)" }}>into</span> Impactful
+              Code
+            </h4>
+            <article
+              className={`${style.projectDetailsMob} ${style.projectDetailsCommon}`}
+            >
+              Each project I build shows my passion for turning ideas into
+              real-world apps. As a 5th-semester CSE student, I focus on clean
+              design and modern tech. I create frontends with{" "}
+              <span className={style.projectDetailHighlights}>
+                React + Tailwind CSS{" "}
+              </span>
+              and backends using{" "}
+              <span className={style.projectDetailHighlights}>Java</span>. My
+              work involves{" "}
+              <span className={style.projectDetailHighlights}>MySQL</span> and{" "}
+              <span className={style.projectDetailHighlights}>responsive</span>,{" "}
+              <span>user-friendly</span>
+              interfaces. Every project moves me closer to becoming a skilled{" "}
+              <span className={style.projectDetailHighlights}>
+                full-stack developer
+              </span>
+              .
+            </article>
+            <article
+              className={`${style.projectDetailsTblet} ${style.projectDetailsCommon}`}
+            >
+              Each project I build reflects my passion for turning ideas into
+              real-world solutions. As a 5th-semester Diploma CSE student, I
+              focus on clean design and strong logic. I develop frontends using{" "}
+              <span className={style.projectDetailHighlights}>React</span> and{" "}
+              <span className={style.projectDetailHighlights}>
+                Tailwind CSS
+              </span>
+              , and backends with{" "}
+              <span className={style.projectDetailHighlights}>Java</span>. My
+              projects also explore{" "}
+              <span className={style.projectDetailHighlights}>
+                MySQL data handling
+              </span>{" "}
+              and smooth{" "}
+              <span className={style.projectDetailHighlights}>
+                UI experiences
+              </span>
+              . Every task enhances my understanding of development and best
+              practices. Each project strengthens my path toward becoming a
+              skilled{" "}
+              <span className={style.projectDetailHighlights}>
+                full-stack developer
+              </span>
+              .
+            </article>
+            <article
+              className={`${style.projectDetailsLpt} ${style.projectDetailsCommon} `}
+            >
+              Each project I build reflects my passion for transforming concepts
+              into real-world applications. As a 5th-semester Diploma CSE
+              student, I focus on clean design, logical thinking, and modern
+              tech. I craft frontends with{" "}
+              <span className={style.projectDetailHighlights}>
+                React + Tailwind CSS
+              </span>{" "}
+              and backends using{" "}
+              <span className={style.projectDetailHighlights}>Java</span>. My
+              work explores{" "}
+              <span className={style.projectDetailHighlights}>
+                MySQL data handling
+              </span>{" "}
+              and{" "}
+              <span className={style.projectDetailHighlights}>
+                responsive, user-friendly
+              </span>{" "}
+              interfaces. I continuously refine my coding style to ensure{" "}
+              <span className={style.projectDetailHighlights}>
+                Extensiblility
+              </span>{" "}
+              and performance. Every project challenges me to learn new tools
+              and{" "}
+              <span className={style.projectDetailHighlights}>
+                problem-solving techniques
+              </span>
+              . Each creation strengthens my journey toward becoming a skilled{" "}
+              <span className={style.projectDetailHighlights}>
+                full-stack developer
+              </span>
+              .
+            </article>
+          </div>
+          <div className="my-5 pb-lg-1">
+            <h4 className={`${style.projectsTitle} ${style.projectsTitleMob}`}>
+              Projects That Define My <br />{" "}
+              <span className={style.projTitleHighLight}>Journey</span>
+            </h4>
+            <h4
+              className={`${style.projectsTitle} ${style.projectsTitleNonMob}`}
+            >
+              Projects That Define My{" "}
+              <span className={style.projTitleHighLight}>Journey</span>
+            </h4>
+          </div>
+        </div>
+      </section>
+      <Divider />{" "}
+    </>
+  );
+};
+export default Project;
