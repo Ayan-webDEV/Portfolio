201ec633e26a7f563d47e4ee8759ee1e8a6e017d
Ayan-webDEV <ayanalamdev@gmail.com>
2025-11-12 00:29:58 +0530

    contact section added

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import style from "./Contact.module.css";
+import Hedding from "../CommonComponent/Hedding";
+
+const Contact = () => {
+  return (
+    <section
+      id="contact"
+      className="container-fluid my-4 pt-3 my-sm-5 mt-lg-4 mb-lg-5 pt-lg-4"
+    >
+      <div className="container">
+        <Hedding hds={"Contact Me"} cursor={"💬"} />
+      </div>
+    </section>
+  );
+};
+export default Contact;
