b0a5700d6af917ca09a9562a5e45d75f69769ee5
Ayan-webDEV <ayanalamdev@gmail.com>
2025-11-12 15:42:09 +0530

    divider added and there is a error on deployment it redeployment of that

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,16 +1,20 @@
 import style from "./Contact.module.css";
 import Hedding from "../CommonComponent/Hedding";
+import Divider from "../CommonComponent/Divider";
 
 const Contact = () => {
   return (
-    <section
-      id="contact"
-      className="container-fluid my-4 pt-3 my-sm-5 mt-lg-4 mb-lg-5 pt-lg-4"
-    >
-      <div className="container">
-        <Hedding hds={"Contact Me"} cursor={"💬"} />
-      </div>
-    </section>
+    <>
+      <section
+        id="contact"
+        className="container-fluid my-4 pt-3 my-sm-5 mt-lg-4 mb-lg-5 pt-lg-4"
+      >
+        <div className="container">
+          <Hedding hds={"Contact Me"} cursor={"💬"} />
+        </div>
+      </section>
+      <Divider />
+    </>
   );
 };
 export default Contact;
